a3c146aaab04beb7da0838e970b1f1156044eeae
Xucong Zhan <hymanzxc@umich.edu>
2020-01-03 22:19:03 -0500

    Added "bookmark = " assignment

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -9,7 +9,7 @@ def test_unbookmark_a_blog(session):
     author = create_random_user(session)
     blog = create_random_blog(session, is_published=True, author_id=author.id)
     bookmarker = create_random_user(session)
-    crud.toggle_bookmark(session, blog_id=blog.id, bookmarker_id=bookmarker.id)
+    bookmark = crud.toggle_bookmark(session, blog_id=blog.id, bookmarker_id=bookmarker.id)
 
     assert len(blog.bookmarks) == 1
 
